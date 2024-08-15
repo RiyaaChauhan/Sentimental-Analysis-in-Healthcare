0dff1161f9d320db515276d837ac1776f66c08ba
rcccola <riyacchauhan12@gmail.com>
2024-08-15 12:59:48 +0530

    more updates comming

---
+++ sentiment.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+plt.style.use('my styling')
+
+import nltk
+
+#reading data
+df= pd.read_csv('./sentiment_data.csv')
